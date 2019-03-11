14b2a388b0788684d276d7b39c958c616ed8502b
Purni <purnia92@gmail.com>
2019-03-11 02:24:40 -0500

    all routes and paths set. storing data in array. need to work on comparison

---
+++ routes/apiRoutes.js
@@ -0,0 +1,40 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on surveys.
+// ===============================================================================
+
+var surveyData = require("../data/friends");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+  
+    app.get("/api/survey", function(req, res) {
+      res.json(surveyData);
+      console.log(surveyData);
+    });
+
+
+    // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/survey", function(req, res) {
+    surveyData.push(req.body);
+  });
+
+};
+
